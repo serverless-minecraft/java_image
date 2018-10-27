021c2c1406dc9b8329a41c373e506628536b43e3
KevinL <kevin@littlejohn.id.au>
2018-10-27 04:12:28 +0000

    invalidate cloudfront on every backup - temporary

--- minecraft-image/dobackup.sh
+++ minecraft-image/dobackup.sh
@@ -5,7 +5,10 @@ zip -r /tmp/$BACKUP_NAME.zip world
 echo "uploading archive to S3"
 aws s3 cp /tmp/*.zip $S3_BUCKET_URL
 echo "removing local archive"
-rm /tmp/*.zip
+rm /tmp/*.
+echo "Invalidate CloudFront"
+DISTRIBUTIONID=`aws cloudfront list-distributions --query 'DistributionList.Items[0].Id' --output text`
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTIONID --paths /index.html
 echo "done"
 
 echo "Checking for logged in users..."
